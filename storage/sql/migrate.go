6c9046d9a13c07bf62f5af3cc4cdc46dc548e341
s10dev <s10.dev@yandex.com>
2025-08-18 20:21:34 +0300

    add 0-6 patches

--- storage/sql/migrate.go
+++ storage/sql/migrate.go
@@ -298,4 +298,25 @@ var migrations = []migration{
 				add column hmac_key bytea;`,
 		},
 	},
+	{
+		stmts: []string{
+			`
+			alter table password
+				add column groups text default "" not null
+			`,
+		},
+	},
+	{
+		stmts: []string{
+			`
+			alter table offline_session
+				add column totp text;`,
+			`
+			alter table offline_session
+				add column totp_confirmed boolean default false;`,
+			`
+			alter table auth_request
+				add column totp_validated boolean not null default false;`,
+		},
+	},
 }
